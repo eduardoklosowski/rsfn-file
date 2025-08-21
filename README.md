79929c5ab2047d789fda9a2c2225258d111128bc
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2025-08-21 02:28:10 +0000

    Cria comando para imprimir o cabeçalho

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Utilitário para Arquivos que Trafegam na RSFN (Rede do Sistema Financeiro Nacional)
+
+## Exemplos de Uso
+
+### Mostrar e Validar o Cabeçalho de Segurança de um Arquivo
+
+```txt
+$ rsfnfile header -i arquivo-criptografado
+================================ BEGIN HEADER ================================
+C01 Tamanho do cabeçalho                     : 0x024c [588]
+C02 Versão do protocolo                      : 0x03 [Terceira versão]
+C03 Código de erro                           : 0x00 [Sem erros]
+C04 Tratamento especial                      : 0x08 [Arquivo compactado]
+C05 Reservado                                : 0x00 [-]
+C06 Algoritmo da chave assimétrica do destino: 0x02 [RSA com 2048 bits]
+C07 Algoritmo da chave simétrica             : 0x02 [AES com 256 bits]
+C08 Algoritmo da chave assimétrica local     : 0x02 [RSA com 2048 bits]
+C09 Algoritmo de hash                        : 0x03 [SHA-256]
+C10 PC do certificado do destino             : 0x01 [SPB-Serpro]
+C11 Série do certificado do destino          : 0x3030303030303030303030303030303030303030303030303030313233343536 [0x123456]
+C12 PC do certificado da instituição         : 0x01 [SPB-Serpro]
+C13 Série do certificado da instituição      : 0x3030303030303030303030303030303030303030303030303030414243444546 [0xabcdef]
+C14 Buffer da chave simétrica                : blob [len=256]
+C15 Buffer da autenticação da mensagem       : blob [len=256]
+================================ END HEADER ================================
+```
--- src/bin/rsfnfile.rs
+++ src/bin/rsfnfile.rs
@@ -1,11 +1,32 @@
 use std::{fs::File, io, path::PathBuf};
 
-use clap::{Command, arg, command, value_parser};
+use clap::{ArgAction, Command, arg, command, value_parser};
 use clap_complete::{Shell, generate};
+use rsfn_file::header::Header;
 
 fn build_cli() -> Command {
     command!("rsfnfile")
         .subcommand_required(true)
+        .subcommand(
+            command!("header")
+                .about("Mostra o cabeçalho de segurança do arquivo da RSFN")
+                .arg(
+                    arg!(-i --in <PATH> "Arquivo de entrada, se não informado o STDIN será lido")
+                        .value_parser(value_parser!(PathBuf))
+                )
+                .arg(
+                    arg!(-o --out <PATH> "Arquivo de saída, se não informado o STDOUT será escrito")
+                        .value_parser(value_parser!(PathBuf))
+                )
+                .arg(
+                    arg!(verbose: -q --quiet "Não imprime o cabeçalho de segurança do arquivo")
+                        .action(ArgAction::SetFalse)
+                )
+                .arg(
+                    arg!(verifyheader: -n --noverifyheader "Não verifica se o cabelhaço de segurança tem valores válidos nos campos")
+                        .action(ArgAction::SetFalse)
+                )
+        )
         .subcommand(
             command!("completion")
                 .about("Gera completion para o shell")
@@ -48,6 +69,29 @@ fn open_output(filepath: Option<&PathBuf>) -> Box<dyn io::Write> {
 
 fn main() -> io::Result<()> {
     match build_cli().get_matches().subcommand() {
+        Some(("header", matches)) => {
+            let mut input = open_input(matches.get_one("in"));
+            let mut output = open_output(matches.get_one("out"));
+
+            let header = match Header::read_from_file(&mut input) {
+                Ok(header) => header,
+                Err(error) => {
+                    eprintln!("ERRO: Falha ao ler cabeçalho de segurança: {error}");
+                    std::process::exit(1);
+                }
+            };
+
+            if matches.get_flag("verbose") {
+                writeln!(output, "{header}")?;
+            }
+            if matches.get_flag("verifyheader")
+                && let Err(error) = header.is_header_valid()
+            {
+                eprintln!("ERRO: Cabeçalho de segurança inválido: {error}");
+                std::process::exit(1);
+            }
+        }
+
         Some(("completion", matches)) => {
             let mut output = open_output(matches.get_one("out"));
 
